51ca07c6536dfdb01b10f53c8ccc30fff0bf61f4
MahdiehSedaghat <mahdie.sedaghat@gmail.com>
2021-07-08 19:46:57 +0430

    fix a small issue in updating an existing session

--- src/utils/strToISO.ts
+++ src/utils/strToISO.ts
@@ -1,5 +1,5 @@
 export function strToISO(dateStr: string, timeStr: string) {
   const [hour, min, sec] = timeStr.split(':');
   const [year, month, day] = dateStr.split('/');
-  return new Date(+year, +month, +day, +hour, +min, +sec).toISOString();
+  return new Date(+year, +month - 1, +day, +hour, +min, +sec).toISOString();
 }
