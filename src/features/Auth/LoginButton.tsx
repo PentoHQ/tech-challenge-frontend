120bd2a186202253839c29da07f7a2329af0d9f9
Mohammadreza <moharnadreza@gmail.com>
2021-10-09 16:25:09 +0330

    refactor: auth feature components

--- src/features/Auth/LoginButton.tsx
+++ src/features/Auth/LoginButton.tsx
@@ -1,17 +1,9 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import styles from './LoginButton.module.scss'
-import logo from '../../logo.svg'
 
-function LoginButton() {
+const LoginButton = (): JSX.Element => {
   const { loginWithRedirect } = useAuth0()
 
-  return (
-    <div className={styles.wrapper}>
-      <img src={logo} className={styles.logo} alt="logo" />
-      <h1>Pento time tracker</h1>
-      <button onClick={loginWithRedirect}>Login</button>
-    </div>
-  )
+  return <button onClick={loginWithRedirect}>Login</button>
 }
 
 export default LoginButton
