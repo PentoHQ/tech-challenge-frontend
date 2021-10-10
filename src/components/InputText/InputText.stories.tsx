7de53b74752228d2648877af3fc2a2e70afe4dc8
Mohammadreza <moharnadreza@gmail.com>
2021-10-10 10:36:54 +0330

    refactor: input text component

--- src/components/InputText/InputText.stories.tsx
+++ src/components/InputText/InputText.stories.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0'
-
-import { InputText, InputProps } from './InputText'
+import { Meta, Story } from '@storybook/react/types-6-0'
+import InputText from './InputText'
+import type { InputProps } from './InputText'
 
 export default {
   title: 'Example/Input',
@@ -13,26 +11,11 @@ export default {
 } as Meta
 
 const Template: Story<InputProps> = (args) => {
-  const [state, setState] = useState('')
-  const props = { ...args, onChange: setState, value: state }
-  return <InputText {...props} />
+  return <InputText {...args} />
 }
 
 export const Primary = Template.bind({})
 Primary.args = {
   label: 'text input',
   placeholder: 'placeholder text',
-}
-
-export const Secondary = Template.bind({})
-Secondary.args = {}
-
-export const Large = Template.bind({})
-Large.args = {
-  size: 'large',
-}
-
-export const Small = Template.bind({})
-Small.args = {
-  size: 'small',
 }
--- src/components/InputText/InputText.tsx
+++ src/components/InputText/InputText.tsx
@@ -1,47 +1,57 @@
-import capitalize from '../../util/capitaize'
+import type { InputHTMLAttributes, Ref } from 'react'
+import { forwardRef } from 'react'
+import capitalize from 'src/util/capitaize'
 import styles from './InputText.module.scss'
 
-export interface InputProps {
-  /**
-   * How large should the input be?
-   */
-  size?: 'small' | 'medium' | 'large'
-  /**
-   * Provide your custom styles by passing a class name that will
-   * be applied to the root of the component (edit to match reality)
-   */
-  className?: string
-  label?: string
-  value: string
-  onChange: (val: string) => any
-  placeholder?: string
-  type?: 'text' | 'search' | 'password'
-}
+export type InputProps = {
+  name: string
+  label: string
+  required?: boolean
+  error?: string
+} & InputHTMLAttributes<HTMLInputElement>
 
-/**
- * Primary UI component for user interaction
- */
-export const InputText = ({
-  size = 'medium',
-  label: text,
-  type = 'text',
-  className = '',
-  placeholder = '',
-  onChange,
-  ...props
-}: InputProps) => {
-  const classes = [styles.wrapper, className].join(' ').trim()
+const InputText = (
+  {
+    name,
+    label,
+    error,
+    disabled,
+    required,
+    type = 'text',
+    placeholder = '',
+    ...restProps
+  }: InputProps,
+  ref: Ref<HTMLInputElement>,
+): JSX.Element => {
+  const inputClasses = `
+    ${styles.input}
+  `
+
   return (
-    <div className={classes}>
-      {text && <label className={styles.label}>{capitalize(text)}</label>}
+    <div className={styles.wrapper}>
+      <label htmlFor={name} className={styles.label}>
+        {capitalize(label)}
+        {required && <sup>&nbsp;*</sup>}
+      </label>
+
       <input
-        {...props}
-        placeholder={capitalize(placeholder)}
+        id={name}
+        name={name}
+        ref={ref}
         type={type}
-        onChange={(e) => onChange(e.target.value)}
+        disabled={disabled}
+        className={inputClasses}
+        placeholder={capitalize(placeholder)}
+        {...restProps}
       />
+
+      {error && !disabled && (
+        <span className={styles.errorMessage} data-testid={`${name}-error`}>
+          {error}
+        </span>
+      )}
     </div>
   )
 }
 
-export default InputText
+export default forwardRef(InputText)
