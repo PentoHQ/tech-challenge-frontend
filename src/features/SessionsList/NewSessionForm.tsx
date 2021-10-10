7c337475e7796844044740b9f995b0f0c054b174
Mohammadreza <moharnadreza@gmail.com>
2021-10-10 11:01:08 +0330

    refactor: new session form component

---
+++ src/features/SessionsList/NewSessionForm.tsx
@@ -0,0 +1,47 @@
+import { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import FormRow from 'src/components/FormRow'
+import InputText from 'src/components/InputText'
+import PlayButton from 'src/components/PlayButton'
+import { ERROR_MESSAGES } from 'src/constants'
+import { useRunningSession } from './hooks'
+
+type FormFields = {
+  sessionName: string
+}
+
+const NewSessionForm = (): JSX.Element => {
+  const { isLoading, startSession } = useRunningSession()
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid, errors },
+  } = useForm<FormFields>({
+    mode: 'all',
+  })
+
+  const onSubmit = useCallback(
+    ({ sessionName }: FormFields) => {
+      startSession(sessionName)
+    },
+    [startSession],
+  )
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow alignY="bottom" stretchLastChild={false}>
+        <InputText
+          label="Session Name"
+          error={errors.sessionName?.message}
+          {...register('sessionName', {
+            required: ERROR_MESSAGES.REQUIRED,
+          })}
+          required
+        />
+        <PlayButton disabled={isLoading || !isValid} type="submit" />
+      </FormRow>
+    </form>
+  )
+}
+
+export default NewSessionForm
