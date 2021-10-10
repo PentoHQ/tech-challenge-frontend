6c5f8d3da5e053aacea1049748bba9af31667222
Mohammadreza <moharnadreza@gmail.com>
2021-10-10 16:10:43 +0330

    refactor: cleanup session feature component

--- src/features/SessionsList/SessionControls.tsx
+++ src/features/SessionsList/SessionControls.tsx
@@ -1,14 +1,13 @@
-import Loading from 'src/components/Loading'
 import { useRunningSession } from './hooks'
 import NewSessionForm from './NewSessionForm'
 import RunningSession from './RunningSession'
 
 const SessionControls = (): JSX.Element => {
-  const { isLoading, runningSession } = useRunningSession()
+  const { runningSession } = useRunningSession()
 
-  if (isLoading) return <Loading />
-
-  if (!runningSession) return <NewSessionForm />
+  if (!runningSession) {
+    return <NewSessionForm />
+  }
 
   return (
     <RunningSession name={runningSession.name} startDate={new Date(runningSession.startDate)} />
