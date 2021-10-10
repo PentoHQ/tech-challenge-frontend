fdc92c57fc565d50cabc93c04f7d2f6c45e509af
Mohammadreza <moharnadreza@gmail.com>
2021-10-10 11:02:01 +0330

    refactor: session controls component

--- src/features/SessionsList/SessionControls.tsx
+++ src/features/SessionsList/SessionControls.tsx
@@ -1,61 +1,18 @@
-import { intervalToDuration } from 'date-fns'
-import { SyntheticEvent, useState } from 'react'
-import FormRow from '../../components/FormRow'
-import InputText from '../../components/InputText'
-import PlayButton from '../../components/PlayButton'
-import StopButton from '../../components/StopButton'
+import Loading from 'src/components/Loading'
 import { useRunningSession } from './hooks'
+import NewSessionForm from './NewSessionForm'
+import RunningSession from './RunningSession'
 
-interface RunningProps {
-  name: string
-  startDate: Date
-}
-
-function RunningSession({ name, startDate }: RunningProps) {
-  const { stop, isLoading } = useRunningSession()
-  const { hours, minutes, seconds } = intervalToDuration({ start: startDate, end: new Date() })
-  return (
-    <FormRow alignY="center" stretchLastChild={false}>
-      {name}
-      <div>
-        {hours}:{minutes}:{seconds}
-      </div>
-      <StopButton onClick={stop} disabled={isLoading}></StopButton>
-    </FormRow>
-  )
-}
-
-export default function SessionControls() {
+const SessionControls = (): JSX.Element => {
   const { isLoading, runningSession } = useRunningSession()
-  if (isLoading)
-    return (
-      <FormRow>
-        <span>Loading</span> ...
-      </FormRow>
-    )
-  return runningSession ? (
+
+  if (isLoading) return <Loading />
+
+  if (!runningSession) return <NewSessionForm />
+
+  return (
     <RunningSession name={runningSession.name} startDate={new Date(runningSession.startDate)} />
-  ) : (
-    <SessionInput />
   )
 }
 
-function SessionInput() {
-  const [sessionName, setSessionName] = useState('')
-  const { isLoading, startSession } = useRunningSession()
-  const submit = (e?: SyntheticEvent) => {
-    e?.preventDefault()
-    if (!sessionName) {
-      return
-    }
-    startSession(sessionName)
-  }
-  return (
-    <form onSubmit={submit}>
-      <FormRow alignY="center" stretchLastChild={false}>
-        <InputText onChange={setSessionName} value={sessionName}></InputText>
-        <PlayButton onClick={submit} disabled={isLoading} />
-      </FormRow>
-    </form>
-  )
-}
+export default SessionControls
